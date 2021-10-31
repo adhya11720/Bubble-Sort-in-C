56a81bd402533b75316faef14794ab9801277ed6
Priyanka Hiranandani <priyankahiranandani@gmail.com>
2021-10-31 07:14:51 +0530

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -4,18 +4,24 @@
 void bubbleSortfunc(int arr[], int n)
 {
     int i, j, temp;
+    int flag = 0;
     for(i = 0; i < n; i++)
     {
+        flag = 0;
         for(j = 0; j < n-i-1; j++)
         {
             if( arr[j] > arr[j+1])
             {
-                 
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             } 
         }
+        if (flag == 0) //in case of all array is sorted 
+        {
+            break;
+        }        
     }
     
      
